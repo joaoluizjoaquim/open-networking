bc6c8080764e6433b9693d35ed2c50ced1a3da1b
João Luiz Monteiro Joaquim <joaoluizmj@gmail.com>
2014-04-07 21:05:23 -0300

    Desenvolvimento do template. Desenvolvimento do managed bean da página de procura de eventos.

---
+++ src/main/java/org/gujavasc/openetworking/bean/Event.java
@@ -0,0 +1,30 @@
+package org.gujavasc.openetworking.bean;
+
+public class Event {
+
+	private String name;
+	
+	private Integer totalParticipants;
+	
+	public Event(String name, Integer totalParticipants) {
+		this.name = name;
+		this.totalParticipants = totalParticipants;
+	}
+
+	public Integer getTotalParticipants() {
+		return totalParticipants;
+	}
+
+	public void setTotalParticipants(Integer totalParticipants) {
+		this.totalParticipants = totalParticipants;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/org/gujavasc/openetworking/mb/EventMB.java
@@ -0,0 +1,37 @@
+package org.gujavasc.openetworking.mb;
+
+import java.io.Serializable;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.gujavasc.openetworking.bean.Event;
+
+@Named
+@ViewScoped
+public class EventMB implements Serializable{
+	
+	private static final long serialVersionUID = -6601391210412051501L;
+	
+	private boolean search;
+	private List<Event> eventsFound;
+	
+	public void searchEvent(){
+		eventsFound = Arrays.asList(new Event("TDC 2014", 5753), new Event("JavaOne",8492));
+		search = true;
+	}
+	
+	public Boolean foundSomeEvent(){
+		return search;
+	}
+
+	public List<Event> getEventsFound() {
+		return eventsFound;
+	}
+	
+}
