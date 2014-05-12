5853a394d9fbc4e7906aeba1b0260523dece556b
João Luiz Monteiro Joaquim <joaoluizmj@gmail.com>
2014-05-12 19:20:37 -0300

    Adição das propriedades referentes a data do evento. Apontar a página para as propriedades do evento selecionado.

--- src/main/java/org/gujavasc/openetworking/bean/Event.java
+++ src/main/java/org/gujavasc/openetworking/bean/Event.java
@@ -1,17 +1,26 @@
 package org.gujavasc.openetworking.bean;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Event {
 
 	private String name;
-	
+
 	private Integer totalParticipants;
-	
+
+	private Date initialDate;
+
+	private Date finalDate;
+
 	public Event() {
 	}
-	
+
 	public Event(String name, Integer totalParticipants) {
 		this.name = name;
 		this.totalParticipants = totalParticipants;
+		this.initialDate = Calendar.getInstance().getTime();
+		this.finalDate   = Calendar.getInstance().getTime(); 
 	}
 
 	public Integer getTotalParticipants() {
@@ -30,4 +39,20 @@ public class Event {
 		this.name = name;
 	}
 	
+	public Date getInitialDate() {
+		return initialDate;
+	}
+
+	public void setInitialDate(Date initialDate) {
+		this.initialDate = initialDate;
+	}
+
+	public Date getFinalDate() {
+		return finalDate;
+	}
+
+	public void setFinalDate(Date finalDate) {
+		this.finalDate = finalDate;
+	}
+
 }
