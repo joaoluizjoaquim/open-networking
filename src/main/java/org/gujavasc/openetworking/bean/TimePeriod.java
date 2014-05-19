c806092f7033cc9e2ba7b1cc0905e66b353789df
João Luiz Monteiro Joaquim <joaoluizmj@gmail.com>
2014-05-19 19:12:28 -0300

    Alteração para criação de template blank. Início do desenvolvimento do converter para a tag input[type="date"]

---
+++ src/main/java/org/gujavasc/openetworking/bean/TimePeriod.java
@@ -0,0 +1,27 @@
+package org.gujavasc.openetworking.bean;
+
+import java.util.Date;
+
+public class TimePeriod {
+
+	private Date initialDate;
+	
+	private Date finalDate;
+
+	public Date getInitialDate() {
+		return initialDate;
+	}
+
+	public void setInitialDate(Date initialDate) {
+		this.initialDate = initialDate;
+	}
+
+	public Date getFinalDate() {
+		return finalDate;
+	}
+
+	public void setFinalDate(Date finalDate) {
+		this.finalDate = finalDate;
+	}
+	
+}
